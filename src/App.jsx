9bcab4305f6636800986997a9d563d8da0b8c815
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-08-11 09:54:10 -0700

    Installed ESLint with Airbnb

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>
+          App is live!
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
