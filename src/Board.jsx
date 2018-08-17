2b2ccaaa3e497b83c52b26ca570d0f55c01d90d9
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-08-17 11:40:01 -0700

    Board now using double map to render Cells

--- src/Board.jsx
+++ src/Board.jsx
@@ -21,21 +21,22 @@ const Board = ({
       <div>
         {gameStatus}
       </div>
-      <div className='row-1'>
-        <Cell value={gameBoard[0][0]} handleClick={handleClick} x={0} y={0} />
-        <Cell value={gameBoard[0][1]} handleClick={handleClick} x={0} y={1} />
-        <Cell value={gameBoard[0][2]} handleClick={handleClick} x={0} y={2} />
-      </div>
-      <div className='row-2'>
-        <Cell value={gameBoard[1][0]} handleClick={handleClick} x={1} y={0} />
-        <Cell value={gameBoard[1][1]} handleClick={handleClick} x={1} y={1} />
-        <Cell value={gameBoard[1][2]} handleClick={handleClick} x={1} y={2} />
-      </div>
-      <div className='row-3'>
-        <Cell value={gameBoard[2][0]} handleClick={handleClick} x={2} y={0} />
-        <Cell value={gameBoard[2][1]} handleClick={handleClick} x={2} y={1} />
-        <Cell value={gameBoard[2][2]} handleClick={handleClick} x={2} y={2} />
-      </div>
+      {gameBoard.map((gameRow, index) => {
+        return (
+          <div className={`row-${index}`}>
+            {gameRow.map((cell, index2) => {
+              return (
+                <Cell
+                  value={gameBoard[index][index2]}
+                  handleClick={handleClick}
+                  x={index}
+                  y={index2}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
       <button type='button' onClick={resetGame}>
         Reset The Game
       </button>
